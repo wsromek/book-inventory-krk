104d988c1ce1b29fd7074672f7c82bd0d2682a15
Wojciech Sromek <wojciech.sromek@schibsted.pl>
2016-03-17 10:15:32 +0100

    env vars added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var stockRepo = require('./repositories/stockRepository');
-var app = require('./app')(stockRepo);
+var app = require('./app')(stockRepo(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/book_inventory_store'));
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Example app listening on port 3000!');
 });
